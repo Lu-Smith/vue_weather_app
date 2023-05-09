90658aefb5f1a9c56b514879baac40665de6db1a
Luna Smith <artistlunasmith@gmail.com>
2023-05-09 09:07:36 +0100

    Updated style in App.vue

--- src/App.vue
+++ src/App.vue
@@ -117,5 +117,13 @@ h1 {
   display: inline-block;
   padding: 10px 25px;
   color: #fff;
+  font-size: 102px;
+  font-weight: 900px;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
