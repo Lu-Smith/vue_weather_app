913c7debda98d94a561fc855cff7a92f8e9f4e1e
Luna Smith <artistlunasmith@gmail.com>
2023-05-09 11:39:26 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,14 @@
         />
       </div>
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <p>{{ query }}</p>
         <div class="location-box">
           <div class="location">
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
-          <div class="date">Monday 20 January 2023</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temperature">{{ weather.main.temp }}℃</div>
+          <div class="temperature">{{ Math.round(weather.main.temp) }}℃</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
           <div class="wind">{{ weather.wind.speed }}</div>
         </div>
@@ -59,6 +58,41 @@ export default {
     setResults(results) {
       this.weather = results;
       console.log(results);
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day}, ${date} ${month} ${year} `;
     },
   },
 };
